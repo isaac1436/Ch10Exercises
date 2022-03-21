26250d7f1ad98724bf645c54d9da7c1169017c37
ISAAC DESMOND NRUEM <isaac1436@student.babcock.edu.ng>
2022-03-21 14:22:27 +0100

    Add project files.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,43 @@
+﻿class Exercise1
+{
+    //an exerise using recursion to print out all possible number combinations of 12345 with repetition allowed
+    static int[] vals;
+    static void Main()
+    {
+        Console.Write("Please enter a number: ");
+        bool errorCatch = int.TryParse(Console.ReadLine(), out int n);
+        if (errorCatch = false)
+        {
+            Console.WriteLine("Invalid Value entered");
+        }
+        else
+        {
+            vals = new int[n];
+            NestedLoops(0);
+        }
+    }
+
+    static void NestedLoops(int k)
+    {
+        if (k == vals.Length)
+        {
+            PrintLoops();
+            return;
+        }
+
+        for (int i = 1; i <= vals.Length; i++)
+        {
+            vals[k] = i;
+            NestedLoops(k+1);
+        }
+    }
+
+    static void PrintLoops()
+    {
+        for (int i = 0; i < vals.Length; i++)
+        {
+            Console.Write(vals[i] + " ");
+        }
+        Console.WriteLine("\n");
+    }
+}
---
+++ Exercise2/Program.cs
@@ -0,0 +1,46 @@
+﻿class Exercise2
+{
+    //an exerise using recursion to print out all possible number combinations of 12345 with repetition allowed
+    static int[] vals;
+    static int k;
+    static void Main()
+    {
+        Console.Write("Please enter the value of \'n\': ");
+        bool errorCatch = int.TryParse(Console.ReadLine(), out int n);
+        Console.Write("\nPlease enter the value of \'k\': ");
+        errorCatch = int.TryParse(Console.ReadLine(), out k);
+        if (errorCatch = false)
+        {
+            Console.WriteLine("Invalid Value entered");
+        }
+        else
+        {
+            vals = new int[n];
+            NestedLoops(0);
+        }
+    }
+
+    static void NestedLoops(int count)
+    {
+        if (count == k)
+        {
+            PrintLoops();
+            return;
+        }
+
+        for (int i = 1; i <= vals.Length; i++)
+        {
+            vals[count] = i;
+            NestedLoops(count + 1);
+        }
+    }
+
+    static void PrintLoops()
+    {
+        for (int i = 0; i < k; i++)
+        {
+            Console.Write(vals[i] + " ");
+        }
+        Console.WriteLine("\n");
+    }
+}
